78818d758a7deba3e17a894d41727a46a7827f0b
ChitawornR <chitaworn.rr@gmail.com>
2024-09-05 02:55:36 +0700

    change navbar to center (mx-auto)

--- script/card_slide.js
+++ script/card_slide.js
@@ -1,45 +1,6 @@
-// var carouselWidth = $('.inner-card-slide')[0].scrollWidth;
-// var cardWidth = $('.carousel-item-card').width();
-// var scrollPosition = 0;
-
-// $('.card-slide-next').on('click',function(){
-//     if(scrollPosition < (carouselWidth - (cardWidth*3))){
-//         scrollPosition += cardWidth;
-//         $('.inner-card-slide').animate({scrollLeft: scrollPosition},600)
-//     }
-// })
-// $('.card-slide-prev').on('click',function(){
-//     if(scrollPosition > 0){
-//         scrollPosition -= cardWidth;
-//         $('.inner-card-slide').animate({scrollLeft: scrollPosition},600)
-//     }
-// })
-
-// var carouselWidth = $('.inner-card-slide')[0].scrollWidth;
-// var cardWidth = $('.carousel-item-card').width();
-// var scrollPosition = 0;
-
-// $('.card-slide-next').on('click', function() {
-//     if (scrollPosition < (carouselWidth - (cardWidth * 3))) {
-//         scrollPosition += cardWidth;
-//     } else {
-//         scrollPosition = 0; // รีเซ็ตไปที่จุดเริ่มต้น
-//     }
-//     $('.inner-card-slide').animate({ scrollLeft: scrollPosition }, 600);
-// });
-
-// $('.card-slide-prev').on('click', function() {
-//     if (scrollPosition > 0) {
-//         scrollPosition -= cardWidth;
-//     } else {
-//         scrollPosition = carouselWidth - (cardWidth * 3); // เลื่อนไปที่จุดสิ้นสุด
-//     }
-//     $('.inner-card-slide').animate({ scrollLeft: scrollPosition }, 600);
-// });
-
 var carouselWidth = $('.inner-card-slide')[0].scrollWidth;
 var cardWidth = $('.carousel-item-card').width();
-var visibleCards = Math.floor($('.inner-card-slide').width() / cardWidth);
+var visibleCards = Math.floor($('.inner-card-slide').width() / cardWidth) + 0.2; // ใส่ 0.2 เพราะให่พอดีขนาดหน้าจอ
 var scrollPosition = 0;
 
 $('.card-slide-next').on('click', function() {
@@ -59,4 +20,3 @@ $('.card-slide-prev').on('click', function() {
     }
     $('.inner-card-slide').animate({ scrollLeft: scrollPosition }, 600);
 });
-
