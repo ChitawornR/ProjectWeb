8a283b670fc29887ce5634311802bcf0c2a7725a
ChitawornR <chitaworn.rr@gmail.com>
2024-09-05 00:16:38 +0700

    update crad_slide.js

--- script/card_slide.js
+++ script/card_slide.js
@@ -1,16 +1,62 @@
+// var carouselWidth = $('.inner-card-slide')[0].scrollWidth;
+// var cardWidth = $('.carousel-item-card').width();
+// var scrollPosition = 0;
+
+// $('.card-slide-next').on('click',function(){
+//     if(scrollPosition < (carouselWidth - (cardWidth*3))){
+//         scrollPosition += cardWidth;
+//         $('.inner-card-slide').animate({scrollLeft: scrollPosition},600)
+//     }
+// })
+// $('.card-slide-prev').on('click',function(){
+//     if(scrollPosition > 0){
+//         scrollPosition -= cardWidth;
+//         $('.inner-card-slide').animate({scrollLeft: scrollPosition},600)
+//     }
+// })
+
+// var carouselWidth = $('.inner-card-slide')[0].scrollWidth;
+// var cardWidth = $('.carousel-item-card').width();
+// var scrollPosition = 0;
+
+// $('.card-slide-next').on('click', function() {
+//     if (scrollPosition < (carouselWidth - (cardWidth * 3))) {
+//         scrollPosition += cardWidth;
+//     } else {
+//         scrollPosition = 0; // รีเซ็ตไปที่จุดเริ่มต้น
+//     }
+//     $('.inner-card-slide').animate({ scrollLeft: scrollPosition }, 600);
+// });
+
+// $('.card-slide-prev').on('click', function() {
+//     if (scrollPosition > 0) {
+//         scrollPosition -= cardWidth;
+//     } else {
+//         scrollPosition = carouselWidth - (cardWidth * 3); // เลื่อนไปที่จุดสิ้นสุด
+//     }
+//     $('.inner-card-slide').animate({ scrollLeft: scrollPosition }, 600);
+// });
+
 var carouselWidth = $('.inner-card-slide')[0].scrollWidth;
 var cardWidth = $('.carousel-item-card').width();
+var visibleCards = Math.floor($('.inner-card-slide').width() / cardWidth);
 var scrollPosition = 0;
 
-$('.card-slide-next').on('click',function(){
-    if(scrollPosition < (carouselWidth - (cardWidth*3))){
+$('.card-slide-next').on('click', function() {
+    if (scrollPosition < (carouselWidth - (cardWidth * visibleCards))) {
         scrollPosition += cardWidth;
-        $('.inner-card-slide').animate({scrollLeft: scrollPosition},600)
+    } else {
+        scrollPosition = 0; // รีเซ็ตไปที่จุดเริ่มต้น
     }
-})
-$('.card-slide-prev').on('click',function(){
-    if(scrollPosition > 0){
+    $('.inner-card-slide').animate({ scrollLeft: scrollPosition }, 600);
+});
+
+$('.card-slide-prev').on('click', function() {
+    if (scrollPosition > 0) {
         scrollPosition -= cardWidth;
-        $('.inner-card-slide').animate({scrollLeft: scrollPosition},600)
+    } else {
+        scrollPosition = carouselWidth - (cardWidth * visibleCards); // เลื่อนไปที่จุดสิ้นสุด
     }
-})
+    $('.inner-card-slide').animate({ scrollLeft: scrollPosition }, 600);
+});
+
